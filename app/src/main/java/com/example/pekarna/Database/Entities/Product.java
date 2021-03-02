ddcb58c93a69f9bfb046b4553de8ea6f73765039
student <lizalove3535@gmail.com>
2021-03-02 09:21:30 +0500

    Что надо ещё блин

--- app/src/main/java/com/example/pekarna/Database/Entities/Product.java
+++ app/src/main/java/com/example/pekarna/Database/Entities/Product.java
@@ -1,5 +1,6 @@
 package com.example.pekarna.Database.Entities;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
@@ -10,18 +11,27 @@ public class Product {
     @PrimaryKey(autoGenerate = true)
     public  int ProductID;
 
+    @ColumnInfo(name = "urlPhotoProduct")
     public String urlPhotoProduct;
+    @ColumnInfo (name = "titleProduct")
     public  String titleProduct;
+    @ColumnInfo (name = "price")
     public double price;
 
     public String  getPrice() {
         DecimalFormat decimalFormat = new DecimalFormat("0.00 Р");
         return decimalFormat.format(price);
     }
+    @ColumnInfo(name = "description")
     public String description;
+    @ColumnInfo (name = "kKal")
     public int kKal;
+    @ColumnInfo (name = "protein")
     public int protein;
+    @ColumnInfo (name = "carbohydrates")
     public int carbohydrates;
+    @ColumnInfo (name ="fat")
     public int fat;
+    @ColumnInfo (name = "productCategoryID")
     public  int productCategoryID;
 }
